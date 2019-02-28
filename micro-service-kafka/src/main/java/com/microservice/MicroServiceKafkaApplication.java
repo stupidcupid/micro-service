a020971f5ff6e7018fb2cd03ff7f14e8d5dde001
nanzhou <976487975@qq.com>
2019-02-28 17:22:02 +0800

    消息总线 Spring Cloud Bus Kafka

---
+++ micro-service-kafka/src/main/java/com/microservice/MicroServiceKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceKafkaApplication.class, args);
+	}
+
+}
